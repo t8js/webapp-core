ef2497bb86ed8a9d91ae87520e4fa77559d5f802
axtk <axtkx@inbox.ru>
2025-09-27 22:44:08 +0300

    rename setup

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -7,7 +7,7 @@ import type { LogEventPayload } from "../types/LogEventPayload";
 import { emitLog } from "./emitLog";
 import { renderStatus } from "./renderStatus";
 
-export function setup(init?: () => void | Promise<void>) {
+export function createApp(init?: () => void | Promise<void>) {
   let app = express();
 
   if (!app.events) app.events = new EventEmitter();
@@ -20,7 +20,7 @@ export function setup(init?: () => void | Promise<void>) {
       let location = `http://${host}:${port}/`;
       let env = `NODE_ENV=${process.env.NODE_ENV}`;
 
-      emitLog(app, `Server running at '${location}' (${env})`);
+      emitLog(app, `Server running at ${location} (${env})`);
     });
   };
 
